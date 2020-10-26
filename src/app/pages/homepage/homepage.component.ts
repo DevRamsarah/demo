aa33ff7b62b9f674af97ca5d2ee570e3661d1d33
Dev2205 <dhramsarah@student.udm.ac.mu>
2020-10-26 10:12:21 +0400

    updated

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import gsap from 'gsap/all'
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
@@ -10,6 +10,15 @@ export class HomepageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+  
   }
 
 }
